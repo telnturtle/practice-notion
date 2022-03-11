958767568cccd911a40cf3406de4e85d2b5521bb
Hwi-yong <telnturtle@gmail.com>
2022-03-11 17:08:16 +0900

    Add "Getting Started" API

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 
 
 Project initial setup by https://losikov.medium.com/part-1-project-initial-setup-typescript-node-js-31ba3aa7fbf1
+
+Notion API: https://developers.notion.com/docs/getting-started
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log('Hello World!!!');
+import S from './secret';
+import { Client } from '@notionhq/client';
+
+const notion = new Client({ auth: S.NOTION_KEY });
+
+const databaseId = S.DATABASE_ID;
+
+const addItem = async (text: string) => {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: databaseId },
+      properties: {
+        title: { title: [{ text: { content: text } }] },
+      },
+    });
+    console.log({ response });
+    console.log('Success! Entry added.');
+  } catch (error) {
+    console.error((error as any).body);
+  }
+};
+
+addItem('Yurts in Big Sur, California');
